e97d062c00bac1c3d74da3b0885f8329d8fe7e37
Leigh Johnson <leigh@bitsy.ai>
2022-07-10 11:51:46 -0700

    fix link gen;

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
+          routeBasePath: '/docs/',
 
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
