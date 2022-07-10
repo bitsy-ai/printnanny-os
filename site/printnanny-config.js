5fe90120b6d30890db20ee87ae914a1a0b91aa10
Leigh Johnson <leigh@bitsy.ai>
2022-07-10 13:00:17 -0700

    add latest release link to sidebar

---
+++ site/printnanny-config.js
@@ -0,0 +1,7 @@
+const printnannyConfig = {
+  version: '0.2.0',
+  versionPretty: 'v0.2.0 (Beryl Kirkstone)',
+  slug: 'release-history/0.2.0-beryl-kirkstone'
+};
+
+module.exports = printnannyConfig;
