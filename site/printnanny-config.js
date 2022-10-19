b0a7f1abce88b2904dffb3469fb27b444da279be
Leigh Johnson <leigh@bitsy.ai>
2022-10-18 19:23:14 -0700

    fix release config

--- site/printnanny-config.js
+++ site/printnanny-config.js
@@ -1,7 +1,7 @@
 const printnannyConfig = {
-  version: '0.3.0',
-  versionPretty: 'v0.3.0 (Cinnabar Kirkstone)',
-  slug: 'release-history/0.3.0-cinnabar-kirkstone'
+  version: '0.4.0',
+  versionPretty: 'v0.4.0 (Dunite Kirkstone)',
+  slug: 'release-history/0.4.0-dunite-kirkstone'
 };
 
 module.exports = printnannyConfig;
