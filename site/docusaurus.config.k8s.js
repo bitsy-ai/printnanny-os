3dc8c9e4b73cf9697620a6124f86447ef12609c6
Leigh Johnson <leigh@bitsy.ai>
2022-11-07 19:48:50 -0800

    update patch notes

--- site/docusaurus.config.k8s.js
+++ site/docusaurus.config.k8s.js
@@ -109,9 +109,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       announcementBar: {
-        id: 'v0_4_2',
+        id: 'v0_4_3',
         content:
-          '<a href="https://printnanny.ai/docs/release-history/0.4.x-dunite-langdale/">PrintNanny OS v0.4.2 is now available!</a>',
+          '<a href="https://printnanny.ai/docs/release-history/0.4.x-dunite-langdale/">PrintNanny OS v0.4.3 is now available!</a>',
         backgroundColor: '#fafbfc',
         textColor: '#091E42',
         isCloseable: false,
