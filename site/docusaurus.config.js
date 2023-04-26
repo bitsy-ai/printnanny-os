d6a7da7011a5a738488fb0a606ff5ac8da5006d0
Leigh Johnson <releases@bitsy.ai>
2023-04-26 11:29:52 -0700

    release v0.6.8 patch notes (#303)

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -82,9 +82,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       announcementBar: {
-        id: 'v0_6_7',
+        id: 'v0_6_8',
         content:
-          '<a href="https://printnanny.ai/docs/release-history/0.6.x-feldspar-langdale/">PrintNanny OS v0.6.7 is now available!</a>',
+          '<a href="https://printnanny.ai/docs/release-history/0.6.x-feldspar-langdale/">PrintNanny OS v0.6.8 is now available!</a>',
         backgroundColor: '#fafbfc',
         textColor: '#091E42',
         isCloseable: false,
